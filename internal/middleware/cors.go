625b2bd1fbccd7e9aa8d933bf58efd044cc7fa5d
Ismailjr10 <suleimanismail369@gmail.com>
2026-07-18 22:34:50 +0100

    fix: add X-Client to CORS allowed headers

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -29,7 +29,7 @@ func CorsGuard(next http.Handler) http.Handler {
 		}
 
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token, X-Client")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// Handle preflight browser checks instantly
